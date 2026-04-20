1f9b199327ceea65e2dce056f370542321252884
gabrielrusanescu <dorttog1@gmail.com>
2026-04-20 15:28:53 +0300

    Update tema.c

--- tema.c
+++ tema.c
@@ -53,8 +53,8 @@ RezultatCautare interclaseaza_rezultate(RezultatCautare stanga, RezultatCautare 
     }
     int i=0, j=0, k=0;
     while(i<stanga.numar_rezultate && j<dreapta.numar_rezultate) {
-        if(stanga.date[i].scor >= dreapta.date[j].scor) rez.date[k++] = stanga.date[i++];
-        else rez.date[k++] = dreapta.date[j++];
+        if(stanga.date[i].scor >= dreapta.date[j].scor) rez.date[k++]=stanga.date[i++];
+        else rez.date[k++]=dreapta.date[j++];
     }
     while(i<stanga.numar_rezultate) rez.date[k++]=stanga.date[i++];
     while(j<dreapta.numar_rezultate) rez.date[k++]=dreapta.date[j++];
@@ -64,48 +64,48 @@ RezultatCautare interclaseaza_rezultate(RezultatCautare stanga, RezultatCautare 
 }
 
 RezultatCautare divide_et_impera(char **cai, int inceput, int sfarsit, const char *termen) {
-    if (inceput == sfarsit) {
-        RezultatCautare rez = {NULL, 0};
-        int scor = calculeaza_relevanta(cai[inceput], termen);
-        if(scor>0) {
-            rez.numar_rezultate = 1;
-            rez.date = malloc(sizeof(RezultatDocument));
-            rez.date[0].cale = malloc(strlen(cai[inceput]) + 1);
+    if(inceput==sfarsit){
+        RezultatCautare rez={NULL, 0};
+        int scor=calculeaza_relevanta(cai[inceput], termen);
+        if(scor>0){
+            rez.numar_rezultate=1;
+            rez.date=malloc(sizeof(RezultatDocument));
+            rez.date[0].cale=malloc(strlen(cai[inceput]) + 1);
             strcpy(rez.date[0].cale, cai[inceput]);
-            rez.date[0].scor = scor;
+            rez.date[0].scor=scor;
         }
         return rez;
     }
-    int mijloc=inceput + (sfarsit-inceput) / 2;
+    int mijloc=inceput+(sfarsit-inceput)/2;
     return interclaseaza_rezultate(divide_et_impera(cai, inceput, mijloc, termen), divide_et_impera(cai, mijloc+1, sfarsit, termen));
 }
 
-void ruleaza_cautare(const char *termen) {
-    FILE *f_lista = fopen("documente.txt", "r");
-    if (!f_lista) return;
+void ruleaza_cautare(const char *termen){
+    FILE *f_lista=fopen("documente.txt", "r");
+    if(!f_lista) return;
     char **cai = NULL; char linie[MAX_CALE]; int nr_doc = 0;
-    while (fgets(linie, sizeof(linie), f_lista)) {
+    while(fgets(linie, sizeof(linie), f_lista)){
         elimina_newline(linie);
-        if (strlen(linie) > 0) {
-            cai = realloc(cai, (nr_doc+1) * sizeof(char*));
-            cai[nr_doc] = malloc(strlen(linie) + 1);
+        if(strlen(linie)>0){
+            cai=realloc(cai, (nr_doc+1) * sizeof(char*));
+            cai[nr_doc]=malloc(strlen(linie) + 1);
             strcpy(cai[nr_doc++], linie);
         }
     }
     fclose(f_lista);
-    RezultatCautare final = {NULL, 0};
-    if (nr_doc > 0) final = divide_et_impera(cai, 0, nr_doc - 1, termen);
-    FILE *f_out = fopen("index.out", "w");
-    if (f_out) {
+    RezultatCautare final={NULL, 0};
+    if(nr_doc>0) final=divide_et_impera(cai, 0, nr_doc - 1, termen);
+    FILE *f_out=fopen("index.out", "w");
+    if(f_out){
         fprintf(f_out, "%s\n", termen);
-        for (int i = 0; i < final.numar_rezultate; i++) {
+        for(int i=0; i<final.numar_rezultate; i++) {
             fprintf(f_out, "%s\n", final.date[i].cale);
             free(final.date[i].cale);
         }
         fclose(f_out);
     }
-    if (final.date) free(final.date);
-    for (int i = 0; i < nr_doc; i++) free(cai[i]);
+    if(final.date) free(final.date);
+    for(int i=0; i<nr_doc; i++) free(cai[i]);
     free(cai);
 }
 
@@ -122,21 +122,21 @@ void valideaza_fisier_output(const char *termen_asteptat, const char **cai_astep
     }
 
     //Verific restul liniilor (caile) intr-un for
-    for(int i = 0; i < nr_cai_asteptate; i++) {
-        assert(fgets(linie, sizeof(linie), f) != NULL);
+    for(int i=0; i<nr_cai_asteptate; i++) {
+        assert(fgets(linie, sizeof(linie), f)!=NULL);
         elimina_newline(linie);
-        assert(strcmp(linie, cai_asteptate[i]) == 0);
+        assert(strcmp(linie, cai_asteptate[i])==0);
     }
 
     //Verific sa nu existe linii extra
-    assert(fgets(linie, sizeof(linie), f) == NULL);
+    assert(fgets(linie, sizeof(linie), f)==NULL);
     fclose(f);
 }
 
 int main() {
-    FILE *f1 = fopen("doc_a.txt", "w"); fprintf(f1, "masina\nmasina\nroata\n"); fclose(f1);
-    FILE *f2 = fopen("doc_b.txt", "w"); fprintf(f2, "masina\n"); fclose(f2);
-    FILE *fl = fopen("documente.txt", "w"); fprintf(fl, "doc_a.txt\ndoc_b.txt\n"); fclose(fl);
+    FILE *f1=fopen("doc_a.txt", "w"); fprintf(f1, "masina\nmasina\nroata\n"); fclose(f1);
+    FILE *f2=fopen("doc_b.txt", "w"); fprintf(f2, "masina\n"); fclose(f2);
+    FILE *fl=fopen("documente.txt", "w"); fprintf(fl, "doc_a.txt\ndoc_b.txt\n"); fclose(fl);
 
     //Test1: Cautare "masina" (apare in ambele, doc_a e primul datorita scorului)
     const char *asteptat1[] = {"doc_a.txt", "doc_b.txt"};
